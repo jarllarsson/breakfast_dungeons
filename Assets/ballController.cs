856b9045b2936f26af09a400056bf632cd964ec4
jarllarsson <jarl.larsson@gmail.com>
2015-02-21 20:33:06 +0100

    controller done

---
+++ Assets/ballController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballController : MonoBehaviour {
+
+    public float m_extrapowerMax;
+    public float m_extrapowerInc;
+    private float m_currentBoost;
+    public float m_extrapowerApplyTime = 0.0f;
+    public float m_extrapowerCooldownTime = 0.0f;
+    public float m_extrapowerApplyTick = 0.0f;
+    public float m_extrapowerCooldownTick = 1.0f;
+    public float m_boostMagnitudeLim = 14.0f;
+
+	// Use this for initialization
+	void Start () {
+        m_extrapowerCooldownTick = m_extrapowerCooldownTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(rigidbody2D.velocity.magnitude);
+	}
+
+    void FixedUpdate()
+    {
+        if (rigidbody2D.velocity.magnitude > m_boostMagnitudeLim)
+        {
+            if (m_extrapowerCooldownTick >= m_extrapowerCooldownTime &&
+                m_extrapowerApplyTick > 0.0f)
+            {
+                m_currentBoost += m_extrapowerInc * Time.deltaTime;
+                rigidbody2D.AddForce(rigidbody2D.velocity.normalized * m_currentBoost);
+                m_extrapowerApplyTick -= Time.deltaTime;
+            }
+            if (m_extrapowerApplyTick<=0.0f)
+            {
+                m_currentBoost = 0.0f;
+                m_extrapowerCooldownTick = 0.0f;
+            }
+        }
+        else
+        {
+            m_extrapowerApplyTick = m_extrapowerApplyTime;
+            m_extrapowerCooldownTick += Time.deltaTime;
+            if (m_extrapowerCooldownTick >= m_extrapowerCooldownTime) m_extrapowerApplyTick = m_extrapowerApplyTime;
+        }
+    }
+}
---
+++ Assets/controller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+    public float m_movePower=1.0f;
+    public float m_tiltMultiplier=1.0f, m_tiltMax=10.0f;
+    public LineRenderer m_lineRenderer;
+    public SpringJoint2D m_spring;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void Update()
+    {
+        m_lineRenderer.SetPosition(0, transform.position);
+        m_lineRenderer.SetPosition(1, m_spring.connectedBody.transform.position);
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        Vector2 dirInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 tilt=new Vector2(Input.acceleration.x, Input.acceleration.y);
+        tilt *= Mathf.Clamp(m_tiltMultiplier * tilt.magnitude, 1.0f, m_tiltMultiplier);
+        tilt.x = Mathf.Clamp(tilt.x, -m_tiltMax, m_tiltMax);
+        tilt.y = Mathf.Clamp(tilt.y, -m_tiltMax, m_tiltMax);
+        Debug.Log(tilt);
+        dirInput += tilt;
+        Debug.DrawLine(transform.position,transform.position + new Vector3(dirInput.x, dirInput.y, 0.0f),Color.white);
+        float dirInputSqrMagnitude = dirInput.sqrMagnitude;
+        rigidbody2D.AddForce(dirInput * m_movePower);
+	}
+
+}
