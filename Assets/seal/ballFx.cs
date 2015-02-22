58d87bd2ecba0da2b47eeb9fb3e7ec2d76cd4d4d
jarllarsson <jarl.larsson@gmail.com>
2015-02-22 18:44:40 +0100

    back texture and ball texture

---
+++ Assets/seal/ballFx.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballFx : MonoBehaviour {
+    public Transform m_ball, m_points;
+	// Use this for initialization
+	void Start () 
+    {
+        rigidbody2D.AddTorque(1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_ball.transform.rotation = Quaternion.identity;
+	}
+}
--- Assets/seal/controller.cs
+++ Assets/seal/controller.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class controller : MonoBehaviour {
     public float m_movePower=1.0f;
-    public float m_tiltMultiplier=1.0f, m_tiltMax=10.0f;
+    public Vector2 m_tiltMultiplier, m_tiltMax;
     public LineRenderer m_lineRenderer;
     public SpringJoint2D m_spring;
     public Transform m_face;
@@ -15,9 +16,20 @@ public class controller : MonoBehaviour {
     public float m_flipSpeed = 10.0f;
     private float m_flipT=1.0f;
     private float m_maxAbsVelMp = 1.0f;
+    float startY;
+    float[] startYList;
+    int startYidx = 0;
+    float calibrationTime = 2.0f;
+    public Text m_calibDbgText, m_tiltTxt;
+    public Vector2 m_outsideHDist;
+    private Vector3 m_startPos;
+    public float m_normalDrag = 5.0f, m_outsideDrag = 30.0f, m_veryOutsideDrag=35.0f;
 	// Use this for initialization
 	void Start () {
 	    m_dirAnimHash=Animator.StringToHash("facing");
+        startY=Input.acceleration.y;
+        startYList = new float[10];
+        m_startPos = transform.position;
 	}
 	
     void Update()
@@ -28,12 +40,12 @@ public class controller : MonoBehaviour {
         m_lineRenderer.SetPosition(1, m_spring.connectedBody.transform.position);
 
         // anims
-        float velMagnitude = rigidbody2D.velocity.magnitude;
+        Vector2 vel=rigidbody2D.velocity;
+        float velMagnitude = vel.magnitude;
         if (Mathf.Abs(velMagnitude) > m_maxAbsVelMp) m_maxAbsVelMp = Mathf.Abs(velMagnitude);
-        if (velMagnitude>0.5f)
+        float dirSign = 1.0f;
+        if (velMagnitude>5.0f)
         {
-            Vector2 vel=rigidbody2D.velocity;
-            float dirSign = 1.0f;
             m_paddle1.m_speedMp = 1.0f + (velMagnitude - 0.5f) * 0.05f;
             m_paddle2.m_speedMp = 1.0f + (velMagnitude - 0.5f) * 0.05f;
             if (vel.y>2.0f)
@@ -53,7 +65,6 @@ public class controller : MonoBehaviour {
                 m_face.localScale = new Vector3(blorp(m_flipT, -vel.x), 1.0f, 1.0f);
             }
             //
-            m_tail.localRotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Rad2Deg * Mathf.Atan2(-vel.y, -vel.x * dirSign));
         }
         else
         {
@@ -61,8 +72,30 @@ public class controller : MonoBehaviour {
                 m_face.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
             else
                 m_face.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            dirSign = m_face.localScale.x;
         }
+        if (velMagnitude > 0.5f)
+            m_tail.localRotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Rad2Deg * Mathf.Atan2(-vel.y, -vel.x * dirSign));
         
+        // if outside
+        Vector3 deltaPosFromStart=transform.position-m_startPos;
+        if (deltaPosFromStart.x > -m_outsideHDist.x && deltaPosFromStart.x < m_outsideHDist.x &&
+            deltaPosFromStart.y > -m_outsideHDist.y && deltaPosFromStart.y < m_outsideHDist.y)
+        {
+            rigidbody2D.drag = m_normalDrag;
+        }
+        else
+        {
+            if (deltaPosFromStart.x > -m_outsideHDist.x - 8.0f && deltaPosFromStart.x < m_outsideHDist.x + 8.0f &&
+            deltaPosFromStart.y > -m_outsideHDist.y - 8.0f && deltaPosFromStart.y < m_outsideHDist.y + 8.0f)
+            {
+                rigidbody2D.drag = m_outsideDrag;
+            }
+            else
+                rigidbody2D.drag = m_veryOutsideDrag;
+        }
+            
+
     }
 
     float blorp(float p_t, float p_dir)
@@ -92,17 +125,33 @@ public class controller : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () 
     {
-        Vector2 dirInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        Vector2 tilt=new Vector2(Input.acceleration.x, Input.acceleration.y);
-        tilt *= Mathf.Clamp(m_tiltMultiplier * tilt.magnitude, 1.0f, m_tiltMultiplier);
-        tilt.x = Mathf.Clamp(tilt.x, -m_tiltMax, m_tiltMax);
-        tilt.y = Mathf.Clamp(tilt.y, -m_tiltMax, m_tiltMax);
-        Debug.Log(tilt);
-        dirInput += tilt;
-        Debug.DrawLine(transform.position,transform.position + new Vector3(dirInput.x, dirInput.y, 0.0f),Color.white);
-        float dirInputSqrMagnitude = dirInput.sqrMagnitude;
-        rigidbody2D.AddForce(dirInput * m_movePower);
-        m_maxAbsVelMp *= 0.95f;
+        //if (calibrationTime<0.0f)
+        {
+            Vector2 dirInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            Vector2 tilt = new Vector2(Input.acceleration.x, Input.acceleration.y - startY);
+            tilt.x *= Mathf.Clamp(m_tiltMultiplier.x * tilt.magnitude, 1.0f, m_tiltMultiplier.x);
+            tilt.y *= Mathf.Clamp(m_tiltMultiplier.y * tilt.magnitude, 1.0f, m_tiltMultiplier.y);
+            tilt.x = Mathf.Clamp(tilt.x, -m_tiltMax.x, m_tiltMax.y);
+            tilt.y = Mathf.Clamp(tilt.y, -m_tiltMax.y, m_tiltMax.y);
+            Debug.Log(tilt);
+            dirInput += tilt;
+            Debug.DrawLine(transform.position, transform.position + new Vector3(dirInput.x, dirInput.y, 0.0f), Color.white);
+            float dirInputSqrMagnitude = dirInput.sqrMagnitude;
+            rigidbody2D.AddForce(dirInput * m_movePower);
+            m_maxAbsVelMp *= 0.95f;
+        }
+        //else
+        //{
+        //    startYidx++;
+        //    calibrationTime -= Time.deltaTime;
+        //    startY = 0.0f;
+        //    foreach (float val in startYList)
+        //        startY += val;
+        //    startY /= startYList.Length;
+        //    startYList[startYidx % startYList.Length] = Input.acceleration.y;
+        //}
+        m_calibDbgText.text = startY.ToString();
+        m_tiltTxt.text = "tilt x: " + Input.acceleration.x.ToString() + "|  tilt y: " + Input.acceleration.y.ToString();
 	}
 
 }
